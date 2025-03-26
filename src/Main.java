dda19655666199a3334a4e212cf97061982428a8
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-03-26 12:31:56 +0530

    complete add-new-student-with-marks function

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static String[] studentIdArray = new String[0];
     public static String[] studentNameArray = new String[0];
 
+    public static int[] marksPrfArray = new int[0];
+    public static int[] marksDbmsArray = new int[0];
+
 
     public static void main(String[] args) {
         promptHomePage();
@@ -81,7 +84,36 @@ public class Main {
         printTitle("ADD NEW STUDENT WITH MARKS");
         printDashes();
 
+        addNewStudentIdAndName();
+        addStudentMarks();
+
+        System.out.println();
+        answerYesOrNo("Student has been added successfully. Do you want to add new student (Y/n): ",2);
+    }
+
+    private static void addStudentMarks() {
         Scanner scanner = new Scanner(System.in);
+
+        int marksPrf, marksDbms;
+        boolean isInvalidMarksPrf, isInvalidMarksDbms;
+
+        do {
+            System.out.print("\nProgramming Fundamental Marks     : ");
+            marksPrf = scanner.nextInt();
+            isInvalidMarksPrf = (marksPrf < 0 || 100 < marksPrf);
+            if (isInvalidMarksPrf) System.out.println("Invalid marks. Please enter correct marks");
+        } while (isInvalidMarksPrf);
+
+        marksPrfArray = addNewElementToArray(marksPrfArray, marksPrf); // add new student PRF marks
+
+        do {
+            System.out.print("Database Management System Marks  : ");
+            marksDbms = scanner.nextInt();
+            isInvalidMarksDbms = (marksDbms < 0 || 100 < marksDbms);
+            if (isInvalidMarksDbms) System.out.println("Invalid marks. Please enter correct marks\n");
+        } while (isInvalidMarksDbms);
+
+        marksDbmsArray = addNewElementToArray(marksDbmsArray, marksDbms); // add new student DBMS marks
     }
 
     private static void promptAddNewStudent() {
@@ -90,6 +122,13 @@ public class Main {
         printTitle("ADD NEW STUDENT");
         printDashes();
 
+        addNewStudentIdAndName();
+
+        System.out.println();
+        answerYesOrNo("Student has been added successfully. Do you want to add new student (Y/n): ",1);
+    }
+
+    private static void addNewStudentIdAndName() {
         Scanner scanner = new Scanner(System.in);
 
         String studentId;
@@ -106,52 +145,22 @@ public class Main {
 
         } while (isExistId);
 
-        addNewStudentId(studentId);
+        studentIdArray = addNewElementToArray(studentIdArray, studentId); // add new student id
 
         System.out.print("Enter Student Name : ");
         String studentName = scanner.next();
-        addNewStudentName(studentName);
-
-        System.out.println();
-        answerYesOrNo("Student has been added successfully. Do you want to add new student (Y/N): ");
-
-    }
-
-     private static void answerYesOrNo(String text) {
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-
-            System.out.print(text);  // Print the question again
-
-            String answer = scanner.next().trim().toUpperCase();
-
-            if ("Y".equals(answer)) {
-                promptAddNewStudent();
-                break;
-            } else if ("N".equals(answer)) {
-                promptHomePage();
-                break;
-            }else{
-                moveCursorUpAndClear();  // Move cursor to top and clear the line
-            }
-        }
-    }
-
-    private static void moveCursorUpAndClear() {
-        System.out.print("\033[F\033[K"); // Move cursor up one line and clear it
-    }
-
-    private static void addNewStudentId(String studentId) {
-        studentIdArray = addNewElementToArray(studentIdArray, studentId);
-    }
-
-    private static void addNewStudentName(String studentName) {
-        studentNameArray = addNewElementToArray(studentNameArray, studentName);
+        studentNameArray = addNewElementToArray(studentNameArray, studentName); // add new student name
     }
 
     private static String[] addNewElementToArray(String[] array, String value) {
         String[] tempArray = new String[array.length + 1];
+        System.arraycopy(array, 0, tempArray, 0, array.length);
+        tempArray[array.length] = value; // Add new element at the last index
+        return tempArray;
+    }
+
+    private static int[] addNewElementToArray(int[] array, int value) {
+        int[] tempArray = new int[array.length + 1];
         System.arraycopy(array, 0, tempArray, 0, array.length);
         tempArray[array.length] = value; // Add new element at the last index
         return tempArray;
@@ -162,6 +171,32 @@ public class Main {
             if (id.equalsIgnoreCase(studentId)) return true;
         }
         return false;
+    }
+
+    private static void answerYesOrNo(String text, int num) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print(text);
+
+            String answer = scanner.next().trim().toUpperCase();
+
+            if ("Y".equals(answer)) {
+                switch(num){
+                    case 1:promptAddNewStudent();break;
+                    case 2: promptAddNewStudentWithMarks();break;
+                }
+            } else if ("N".equals(answer)) {
+                promptHomePage();
+                break;
+            } else {
+                moveCursorUpAndClear();  // Move cursor to top and clear the line
+            }
+        }
+    }
+
+    private static void moveCursorUpAndClear() {
+        System.out.print("\033[F\033[K"); // Move cursor up one line and clear it
     }
 
     public static void promptHomePage() {
